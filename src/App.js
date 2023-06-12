92a5953cd170865791d908a28dcd10ea495ba530
Liknx <liknx@hotmail.com>
2023-06-11 21:01:56 -0500

    110620232101

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className='aplicaicon-tareas'>
+    <div className='aplicacion-tareas'>
       <div className='freecodecamp-logo-contenedor'>
         <img
           src={freeCodeCampLogo}
-          className=''
-        />
+          className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
       </div>
     </div>
   );
